2dbafd73f98432ace9535c730dcf08f963ac7886
ander <smith@e-cyber.info>
2015-06-21 18:11:50 +0900

    Unity/チュートリアルをもうちょい進める。ラケットを動かすところまで。 ※ あいかわらず、MonoDevelopではビルドエラー。どういうことだ？

---
+++ unity/Tutorial_1/Assets/RacketController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var Accel = 100.0;
+
+function Start () {
+}
+
+function Update () {
+	GetComponent.<Rigidbody>().AddForce(
+		transform.right * Input.GetAxisRaw( "Horizontal" ) *Accel,
+		ForceMode.Impulse
+	);
+
+}
